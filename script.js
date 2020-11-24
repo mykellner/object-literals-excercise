21d31933fc64c4cebb06911bcdddfd7e30fb3c33
My <mykellner@gmail.com>
2020-11-24 09:43:14 +0100

    Added more example, add property, remove property, update property.

--- script.js
+++ script.js
@@ -16,4 +16,21 @@ let maria = {
     gender: "female"
 }
 
-console.log(niklas, maria);
+// Lägga till egenskaper genom punktnotation. 
+
+niklas.address = "Drottninggatan 20";
+
+// ändra egenskaper i ett obbjekt på två sätt, första är via punktnotation. 
+
+niklas.name = "Johan";
+
+// det andra sättet är på ett arrayliknande sätt.
+
+niklas["age"] = 45;
+
+// Ta bort egenskap, vilket man sällan gör, brukar oftast sätta värdet till null eller 0.
+
+delete niklas.age;
+delete niklas["address"]
+
+console.log(niklas);
