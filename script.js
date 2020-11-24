b3a212277a08517a994d5051aac3f59c11d13916
My <mykellner@gmail.com>
2020-11-24 09:28:56 +0100

    Skapat inledande exempel på objekt.

---
+++ script.js
@@ -0,0 +1,19 @@
+let name = "Niklas";
+let age = 30;
+let gender = "Male";
+
+// klumpa ihop variablerna som hör ihop till ett och samma objekt
+
+let niklas = {
+    name: "Niklas",
+    age: 30,
+    gender: "Male"
+};
+
+let maria = {
+    name: "Maria",
+    age: 28,
+    gender: "female"
+}
+
+console.log(niklas, maria);
